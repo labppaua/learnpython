4926915d00bdb63fb4dc8a0368fa8f66e613cfc5
ppa <lab_ppa@mail.ru>
2020-10-23 13:05:43 +0300

    done 6 task

--- lesson3_task6.py
+++ lesson3_task6.py
@@ -1,3 +1,22 @@
 # Урок 3 задание 6
 # Реализовать функцию int_func(), принимающую слово из маленьких латинских букв и возвращающую его же, но с прописной первой буквой. Например, print(int_func(‘text’)) -> Text.
 
+
+def wd_ch_up(s):
+    """переводит первый символ слова либо строки в верхний регистр, можно использовать .title() либо .capitalize() но функция не для легких путей"""
+    a = s[0].upper()
+    b = s[1:]
+    return a + b
+
+list_a = input("Введите список слов через пробел для получения чудо-реультата ")
+list_a = list_a.split()
+f = len(list_a)
+i = 0
+list_b = ""
+while i < f:
+    word = list_a[i]
+    word = wd_ch_up(word)
+    list_b = str(list_b) + word + " "
+    i = i + 1
+list_b = list_b[:-1]
+print(list_b)
