d3496f54a5ab66b0d6e51c56846ffca1b9fd9509
ppa <lab_ppa@mail.ru>
2020-10-27 08:17:49 +0300

    done 5 task

--- lesson4_task5.py
+++ lesson4_task5.py
@@ -2,3 +2,10 @@
 # Реализовать формирование списка, используя функцию range() и возможности генератора. В список должны войти четные числа от 100 до 1000 (включая границы). Необходимо получить результат вычисления произведения всех элементов списка.
 
 
+from functools import reduce
+
+
+#new_list = [el for el in range(100,1001) if el % 2 == 0]
+#new_list = reduce((lambda a,b: a + b),new_list)
+new_list = reduce((lambda a,b: a + b),[el for el in range(100,1001) if el % 2 == 0])
+print(new_list)
